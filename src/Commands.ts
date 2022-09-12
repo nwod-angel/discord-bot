a5f22e22cba60107e1b96dc00e5d794b1770d161
James Hurburgh <JamesHurburgh@gmail.com>
2022-09-12 19:15:52 +1000

    Feat: Enabled spell command

--- src/Commands.ts
+++ src/Commands.ts
@@ -2,9 +2,11 @@ import { Command } from "./Command.js";
 import { Hello } from "./commands/Hello.js";
 import { Roll } from "./commands/Roll.js";
 import { Goodbye } from "./commands/Goodbye.js";
+import { Spell } from "./commands/Spell.js";
 
 export const Commands: Command[] = [
     Hello,
     Roll,
-    Goodbye
+    Goodbye,
+    Spell
 ];
