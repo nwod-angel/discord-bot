e5c5f50ba4af55b9359c87c41f87e8043f61aea5
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-23 08:33:26 +1000

    fix: Updated Spell Arcana dots

--- src/autoCompleteCommands/SpellAutocomplete.ts
+++ src/autoCompleteCommands/SpellAutocomplete.ts
@@ -2,8 +2,9 @@ import { Client, AutocompleteInteraction, ApplicationCommandOptionChoiceData } f
 import { SpellCommand } from "../commands/SpellCommand.js";
 import { AutoCompleteCommand } from "../AutoCompleteCommand.js";
 import spells from "../data/spells.js";
-import { SymbolLibrary } from "../SymbolLibrary.js"
-const DOT = SymbolLibrary.DotLargeWhite
+import { NwodSymbols } from "@nwod-angel/nwod-core";
+// import { SymbolLibrary } from "../SymbolLibrary.js"
+// const DOT = SymbolLibrary.DotLargeWhite
 
 export const SpellAutocomplete: AutoCompleteCommand = {
     name: SpellCommand.name,
@@ -12,13 +13,14 @@ export const SpellAutocomplete: AutoCompleteCommand = {
         let option = interaction.options.data.filter(d => d.focused)[0]
         if(!option) return []
         let value = interaction.options.get(option.name)!.value?.toString() || ''
+        const symbols = new NwodSymbols()
 
         return spells
             .filter(s => s.name.toLowerCase().includes(value.toLowerCase()))
             .slice(0, this.maxResponses)
             .map(s => 
                 ({
-                    name: `${s.name} (${s.primaryArcana} ${DOT.repeat(parseInt(s.requirements.filter(r => r.name.toLowerCase() === s.primaryArcana.toLowerCase())[0].dots!))})`,
+                    name: `${s.name} (${s.primaryArcana} ${symbols.SpellArcanaDots.repeat(parseInt(s.requirements.filter(r => r.name.toLowerCase() === s.primaryArcana.toLowerCase())[0].dots!))})`,
                     value: s.name
                 })) as unknown as ApplicationCommandOptionChoiceData<string | string>[];
     }
