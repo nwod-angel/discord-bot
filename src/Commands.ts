e725f9913efebf20a4e5c6f15a7ad67316e26145
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-23 08:36:07 +1000

    feat: Added Rule command to list of commands

--- src/Commands.ts
+++ src/Commands.ts
@@ -4,11 +4,13 @@ import { Roll } from "./commands/Roll.js";
 import { Goodbye } from "./commands/Goodbye.js";
 import { SpellCommand } from "./commands/SpellCommand.js";
 import { MeritCommand } from "./commands/MeritCommand.js";
+import { RuleCommand } from "./commands/RuleCommand.js";
 
 export const Commands: Command[] = [
     Hello,
     Roll,
     Goodbye,
     SpellCommand,
-    MeritCommand
+    MeritCommand,
+    RuleCommand
 ];
