61eec05ffb647a6eb518d66687a817b9558b014f
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-23 08:48:35 +1000

    fix: Added Rule autocomplete

--- src/AutoCompleteCommands.ts
+++ src/AutoCompleteCommands.ts
@@ -1,8 +1,10 @@
 import { AutoCompleteCommand } from "./AutoCompleteCommand.js";
 import { MeritAutocomplete } from "./autoCompleteCommands/MeritAutoComplete.js";
+import { RuleAutocomplete } from "./autoCompleteCommands/RuleAutocomplete.js";
 import { SpellAutocomplete } from "./autoCompleteCommands/SpellAutocomplete.js";
 
 export const AutoCompleteCommands: AutoCompleteCommand[] = [
     SpellAutocomplete,
-    MeritAutocomplete
+    MeritAutocomplete,
+    RuleAutocomplete
 ];
