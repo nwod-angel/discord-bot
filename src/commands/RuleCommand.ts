8be5721e9edaceeaba52ab5cc04d2c79cf4f70c1
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-23 18:10:51 +1000

    feat: Added default table

--- src/commands/RuleCommand.ts
+++ src/commands/RuleCommand.ts
@@ -3,6 +3,7 @@ import { Command } from "../Command.js"
 import DiscordChannelLogger from "../DiscordChannelLogger.js"
 import RuleProvider from "../data/RuleProvider"
 import { NwodSymbols } from "@nwod-angel/nwod-core"
+import { Table } from "embed-table"
 
 export const RuleCommand: Command = {
     name: "rule",
@@ -13,7 +14,7 @@ export const RuleCommand: Command = {
             "name": "name",
             "description": "The name of the rule",
             "type": 3, // String
-            "autocomplete" : true
+            "autocomplete": true
         },
         {
             "name": "description",
@@ -22,46 +23,59 @@ export const RuleCommand: Command = {
         }
     ],
     run: async (client: Client, interaction: CommandInteraction) => {
-        await DiscordChannelLogger.setClient(client).logBaggage({interaction: interaction, options: interaction.options})
+        await DiscordChannelLogger.setClient(client).logBaggage({ interaction: interaction, options: interaction.options })
 
         const symbols = new NwodSymbols()
 
         let name: string | undefined = undefined
-        if(interaction.options.get('name')){
+        if (interaction.options.get('name')) {
             name = interaction.options.get('name')!.value?.toString()
         }
 
         let description: string | undefined = undefined
-        if(interaction.options.get('description')){
+        if (interaction.options.get('description')) {
             description = interaction.options.get('description')!.value?.toString()
         }
 
         let rules = RuleProvider.getRules(name, description)
-        if(rules.filter(rule => rule.name.toLowerCase() === name!.toLowerCase()).length === 1) {
+        if (rules.filter(rule => rule.name.toLowerCase() === name!.toLowerCase()).length === 1) {
             rules = rules.filter(rule => rule.name.toLowerCase() === name!.toLowerCase())
         }
 
         let embed = new EmbedBuilder()
-            .setFooter({ 
-                text: interaction.id, 
+            .setFooter({
+                text: interaction.id,
                 // iconURL: 'https://i.imgur.com/AfFp7pu.png'
             })
 
-        if(rules.length === 0) {
+        if (rules.length === 0) {
             await interaction.followUp({
                 ephemeral: true,
                 content: `No rules found.`
             })
-        } else if(rules.length === 1){
+        } else if (rules.length === 1) {
             let rule = rules[0]
             embed.setTitle(rule.name)
             let description = ""
 
             rule.paragraphs.forEach(paragraph => {
-                if(paragraph.example) {
+                if (paragraph.example) {
                     embed.addFields({ name: 'Example', value: `*${paragraph.text}*`, inline: false })
-                } else if(paragraph.prefix) {
+                } else if (paragraph.prefix) {
                     embed.addFields({ name: paragraph.prefix, value: paragraph.text, inline: false })
+                } else if (paragraph.table) {
+                    const table = new Table({
+                        titles: ['Level', 'Money', 'Wins'],
+                        titleIndexes: [0, 8, 16],
+                        columnIndexes: [0, 6, 14],
+                        start: '`',
+                        end: '`',
+                        padEnd: 0
+                    });
+                    table.addRow(['1', '$120', '2'], { override: 4 });
+                    table.addRow(['72', '$10', '25'], { override: 0 });
+                    table.addRow(['614', '$1220', '12']);
+                    embed.addFields(table.toField())
                 } else {
                     embed.addFields({ name: '\u200b', value: paragraph.text, inline: false })
                 }
@@ -74,19 +88,19 @@ export const RuleCommand: Command = {
 
             embed.addFields({ name: 'Sources', value: rule.sourcesString(), inline: false })
         } else {
-            let rulesToDisplay = rules.slice(0,25)
+            let rulesToDisplay = rules.slice(0, 25)
             let ruleTitles = rulesToDisplay.map(s => s.name).join('\n')
             let parameters = ''
 
             embed
-            .setTitle(`Showing ${rulesToDisplay.length} of ${rules.length}`)
-            .addFields(
-                { name: `Showing ${rulesToDisplay.length} of ${rules.length}`, value: ruleTitles, inline: false },
-            )
+                .setTitle(`Showing ${rulesToDisplay.length} of ${rules.length}`)
+                .addFields(
+                    { name: `Showing ${rulesToDisplay.length} of ${rules.length}`, value: ruleTitles, inline: false },
+                )
 
         }
 
-        await DiscordChannelLogger.setClient(client).logBaggage({interaction: interaction, embed: embed})
+        await DiscordChannelLogger.setClient(client).logBaggage({ interaction: interaction, embed: embed })
         await interaction.followUp({
             ephemeral: true,
             embeds: [embed],
