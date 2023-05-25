e2afe417a889eb2cf15984f9517d8e390779dae9
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-26 06:58:28 +1000

    chore: Organised rules and added two

--- src/data/rules.ts
+++ src/data/rules.ts
@@ -1,33 +1,30 @@
-import CombatCharging from "./rules/CombatCharging";
-import CombatDelayingActions from "./rules/CombatDelayingActions";
-import CombatFlankAndRearAttacks from "./rules/CombatFlankAndRearAttacks";
-import CombatGoingProne from "./rules/CombatGoingProne";
-import CombatInitiative from "./rules/CombatInitiative";
-import CombatMovement from "./rules/CombatMovement";
-import CombatSurprise from "./rules/CombatSurprise";
-import CombatUnarmed from "./rules/CombatUnarmed";
-import CombatUnarmedGrapple from "./rules/CombatUnarmedGrapple";
-import Defense from "./rules/Defense";
-import DrawbacksOfGnosis from "./rules/DrawbacksOfGnosis";
-import EffectsOfGnosis from "./rules/EffectsOfGnosis";
-import Health from "./rules/Health";
-import Initiative from "./rules/Initiative";
-import MageSight from "./rules/MageSight";
-import PoisonsAndToxins from "./rules/PoisonsAndToxins";
-import RoteSpecialties from "./rules/RoteSpecialties";
-import Speed from "./rules/Speed";
-import UnseenSenses from "./rules/UnseenSenses";
+import CombatCharging from "./rules/wod/CombatCharging";
+import CombatDelayingActions from "./rules/wod/CombatDelayingActions";
+import CombatFlankAndRearAttacks from "./rules/wod/CombatFlankAndRearAttacks";
+import CombatGoingProne from "./rules/wod/CombatGoingProne";
+import CombatInitiative from "./rules/wod/CombatInitiative";
+import CombatMovement from "./rules/wod/CombatMovement";
+import CombatSurprise from "./rules/wod/CombatSurprise";
+import CombatUnarmed from "./rules/wod/CombatUnarmed";
+import CombatUnarmedGrapple from "./rules/wod/CombatUnarmedGrapple";
+import Defense from "./rules/wod/Defense";
+import DrawbacksOfGnosis from "./rules/mtaw/DrawbacksOfGnosis";
+import EffectsOfGnosis from "./rules/mtaw/EffectsOfGnosis";
+import Health from "./rules/wod/Health";
+import Initiative from "./rules/wod/Initiative";
+import MageSight from "./rules/mtaw/MageSight";
+import PoisonsAndToxins from "./rules/wod/PoisonsAndToxins";
+import RoteSpecialties from "./rules/mtaw/RoteSpecialties";
+import Speed from "./rules/wod/Speed";
+import UnseenSenses from "./rules/mtaw/UnseenSenses";
+import ScrutinizingBeingsAndObjects from "./rules/mtaw/ScrutinizingBeingsAndObjects";
+import UnvielingResonance from "./rules/mtaw/UnvielingResonance";
 
 export default [
     Defense,
     Health,
     Initiative,
     PoisonsAndToxins,
-    RoteSpecialties,
-    EffectsOfGnosis,
-    DrawbacksOfGnosis,
-    MageSight,
-    UnseenSenses,
     CombatDelayingActions,
     CombatInitiative,
     CombatSurprise,
@@ -37,5 +34,14 @@ export default [
     CombatFlankAndRearAttacks,
     CombatGoingProne,
     CombatMovement,
-    Speed
+    Speed,
+
+    // MtAw
+    ScrutinizingBeingsAndObjects,
+    UnvielingResonance,
+    RoteSpecialties,
+    EffectsOfGnosis,
+    DrawbacksOfGnosis,
+    MageSight,
+    UnseenSenses,
 ]
