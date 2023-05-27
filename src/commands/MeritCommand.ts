4daf317f315602864588daa0b4ed6d784934ebdc
James Hurburgh <JamesHurburgh@gmail.com>
2023-05-27 15:27:34 +1000

    feat: Made merit feedback work

--- src/commands/MeritCommand.ts
+++ src/commands/MeritCommand.ts
@@ -13,7 +13,7 @@ export const MeritCommand: Command = {
             "name": "name",
             "description": "The name of the merit",
             "type": 3, // String
-            "autocomplete" : true
+            "autocomplete": true
         },
         {
             "name": "description",
@@ -22,42 +22,42 @@ export const MeritCommand: Command = {
         }
     ],
     run: async (client: Client, interaction: CommandInteraction) => {
-        await DiscordChannelLogger.setClient(client).logBaggage({interaction: interaction, options: interaction.options})
+        await DiscordChannelLogger.setClient(client).logBaggage({ interaction: interaction, options: interaction.options })
 
         const symbols = new NwodSymbols()
 
         let name: string | undefined = undefined
-        if(interaction.options.get('name')){
+        if (interaction.options.get('name')) {
             name = interaction.options.get('name')!.value?.toString()
         }
 
         let description: string | undefined = undefined
-        if(interaction.options.get('description')){
+        if (interaction.options.get('description')) {
             description = interaction.options.get('description')!.value?.toString()
         }
 
         let merits = MeritProvider.getMerits(name, description)
-        if(merits.filter(merit => merit.name.toLowerCase() === name!.toLowerCase()).length === 1) {
+        if (merits.filter(merit => merit.name.toLowerCase() === name!.toLowerCase()).length === 1) {
             merits = merits.filter(merit => merit.name.toLowerCase() === name!.toLowerCase())
         }
 
         let embed = new EmbedBuilder()
-            .setFooter({ 
-                text: interaction.id, 
+            .setFooter({
+                text: interaction.id,
                 // iconURL: 'https://i.imgur.com/AfFp7pu.png'
             })
 
-        if(merits.length === 0) {
+        if (merits.length === 0) {
             await interaction.followUp({
                 ephemeral: true,
                 content: `No merits found.`
             })
-        } else if(merits.length === 1){
+        } else if (merits.length === 1) {
             let merit = merits[0]
             embed.setTitle(merit.titleString())
 
-            if(merit.hasRequirements()) {
-                embed.addFields( { name: 'Requirements', value: merit.requirementsString(), inline: false } )
+            if (merit.hasRequirements()) {
+                embed.addFields({ name: 'Requirements', value: merit.requirementsString(), inline: false })
             }
 
             let descriptionChunks = merit.description.match(/.{1,1000}/g) || []
@@ -79,38 +79,44 @@ export const MeritCommand: Command = {
 
             embed.addFields({ name: 'Sources', value: merit.sourcesString(), inline: false })
         } else {
-            let meritsToDisplay = merits.slice(0,25)
+            let meritsToDisplay = merits.slice(0, 25)
             let meritTitles = meritsToDisplay.map(s => s.titleString()).join('\n')
             let parameters = ''
 
             embed
-            .setTitle(`Showing ${meritsToDisplay.length} of ${merits.length}`)
-            .addFields(
-                { name: `Showing ${meritsToDisplay.length} of ${merits.length}`, value: meritTitles, inline: false },
-            )
+                .setTitle(`Showing ${meritsToDisplay.length} of ${merits.length}`)
+                .addFields(
+                    { name: `Showing ${meritsToDisplay.length} of ${merits.length}`, value: meritTitles, inline: false },
+                )
 
         }
-        
-        // Get feedback
-		const feedbackRow = new ActionRowBuilder<ButtonBuilder>()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId('helpful')
-                .setLabel('Helpful')
-                .setStyle(ButtonStyle.Success),
-            new ButtonBuilder()
-                .setCustomId('unhelpful')
-                .setLabel('Unhelpful')
-                .setStyle(ButtonStyle.Danger),
-        );
 
-        await DiscordChannelLogger.setClient(client).logBaggage({interaction: interaction, embed: embed})
-        await interaction.followUp({
-            ephemeral: true,
+        // Get feedback
+        const actionRow = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('notGood')
+                    .setLabel('Not good')
+                    .setStyle(ButtonStyle.Danger)
+                    .setEmoji("😦")
+            );
+
+        await DiscordChannelLogger.setClient(client).logBaggage({ interaction: interaction, embed: embed })
+        const responseInteraction = await interaction.followUp({
             embeds: [embed],
-            components: [feedbackRow]
+            components: [actionRow]
         });
-		// await interaction.reply({ content: 'Was this helpful?', ephemeral: true, components: [feedbackRow] });
+
+        try {
+            const response = await responseInteraction.awaitMessageComponent({ filter: i => i.user.id === interaction.user.id, time: 10000 })
+            if (response.customId === 'notGood') {
+                console.log(`${interaction.user.username} thinks ${interaction.id} is not good.`)
+            }
+        } catch (e) {
+            // No response
+            await interaction.editReply({ embeds: [embed] });
+        }
+        // await interaction.reply({ content: 'Was this helpful?', ephemeral: true, components: [feedbackRow] });
 
     }
 };
