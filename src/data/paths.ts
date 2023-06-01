f959e141e3724c4abfb3ba1230e7520b712b27e7
James Hurburgh <JamesHurburgh@gmail.com>
2023-06-01 22:36:25 +1000

    fix: Added path data

--- src/data/paths.ts
+++ src/data/paths.ts
@@ -2,52 +2,164 @@ export default [
     {
         pathId: 'acanthus',
         fancyName: '🎲 Acanthus ⌛',
-        realm: 'Arcadia',
+        name: "Acanthus",
+        aka: ["Enchanters"],
+        path: "Thistle",
+        watchtower: "Lunargent Thorn",
+        realm: "Acadia",
+        kingdom: "Enchantment",
+        abode: "Fae",
+        favouredResistance: "Composure",
+        rulingArcana: ["Time", "Fate"],
+        inferiorArcana: "Forces",
+        concepts: [
+            "Charlatan",
+            "Karmic enforcer",
+            "Schizophrenic",
+            "Contemporary bard",
+            "Gypsy prophet",
+            "Lucky ne’er-do-well"
+        ],
+        summary: "Relying on luck and intuition to guide their way",
+        magicalTools: {
+            wands: ["Glass", "Crystal", "Plastic", "Silver"],
+            weapons: ["Knife", "Rapier", "Bow and arrow"]
+        },
         anomalyDescription: '* Ill-luck taints the scene, causing everyone there to suffer a reverse of a rote action effect (see pp. 134-135 of the World of Darkness Rulebook) on their rolls: re-roll successes once, not to add new successes to the total but to replace rolled successes if the second roll results in failures. For example, a roll results in three successes and two failures. The successes are re-rolled, and this time the dice yield only two successes.' + '\n' +
-        '* People get a powerful sense of déjà vu over and over again. They repeat tasks they just accomplished unless they succeed in a Wits + Composure roll.' + '\n' +
-        '* People and/or things move in slow motion while time outside the area of the Intrusion passes normally.' + '\n' +
-        '* Forces spells suffer a –2 penalty (Forces is Arcadia’s Inferior Arcanum).'
+            '* People get a powerful sense of déjà vu over and over again. They repeat tasks they just accomplished unless they succeed in a Wits + Composure roll.' + '\n' +
+            '* People and/or things move in slow motion while time outside the area of the Intrusion passes normally.' + '\n' +
+            '* Forces spells suffer a –2 penalty (Forces is Arcadia’s Inferior Arcanum).'
     },
     {
         pathId: 'mastigos',
         fancyName: '🧠 Mastigos 🌌',
-        realm: 'Pandemonium',
-        anomalyDescription: '& Repressed or denied thoughts and emotions rise up and plague everyone’s mind. Any Social rolls suffer a –2 dice penalty.' + '\n' +
-        '* People become lost and disoriented easily (–2 on any Survival rolls to orient oneself).' + '\n' +
-        '* Objects sometimes roll uphill or sideways.' + '\n' +
-        '* Destinations that are close take longer to get to than places farther away, as if they were at least twice the distance apart.' + '\n' +
-        '* Matter spells suffer a –2 penalty (Death is Pandemonium’s Inferior Arcanum).'
+        name: "Mastigos",
+        aka: ["Warlocks"],
+        path: "Scourging",
+        watchtower: "Iron Gauntlet",
+        realm: "Pandemonium",
+        kingdom: "Nightmares",
+        abode: "Demons",
+        favouredResistance: "Resolve",
+        rulingArcana: ["Space", "Mind"],
+        inferiorArcana: "Matter",
+        concepts: [
+            "Creepy infernalist",
+            "Itinerant demon slayer",
+            "Struggling antihero",
+            "Unseen assassin",
+            "Haunter-of-nightmares",
+            "Real politik diplomat",
+            "Carnival dream interpreter"
+        ],
+        summary: "Exulting in unfettered will",
+        magicalTools: {
+            wands: ["Iron", "Brass", "Leather"],
+            weapons: ["Knife", "Scimitar (or other curved sword such as a katana)", "Whip"]
+        },
+        anomalyDescription: '* Repressed or denied thoughts and emotions rise up and plague everyone’s mind. Any Social rolls suffer a –2 dice penalty.' + '\n' +
+            '* People become lost and disoriented easily (–2 on any Survival rolls to orient oneself).' + '\n' +
+            '* Objects sometimes roll uphill or sideways.' + '\n' +
+            '* Destinations that are close take longer to get to than places farther away, as if they were at least twice the distance apart.' + '\n' +
+            '* Matter spells suffer a –2 penalty (Death is Pandemonium’s Inferior Arcanum).'
     },
     {
         pathId: 'moros',
         fancyName: '💀 Moros 🧱',
-        realm: 'Stygia',
+        name: "Moros",
+        aka: ["Necromancers"],
+        path: "Doom",
+        watchtower: "Lead Coin",
+        realm: "Stygia",
+        kingdom: "Crypts",
+        abode: "Shades",
+        favouredResistance: "Composure",
+        rulingArcana: ["Matter", "Death"],
+        inferiorArcana: "Spirit",
+        concepts: [
+            "Thanatologist",
+            "Speaker for the dead",
+            "Modern alchemist",
+            "Parapsychologist",
+            "Elite assassin",
+            "Leader of a ghostly spy ring",
+            "Dangerous death cultist",
+            "Euthanasia activist",
+            "Inquisitive vivisectionist"
+        ],
+        summary: "Remaining steadfast during change",
+        magicalTools: {
+            wands: ["Lead", "Bone", "Precious gems"],
+            weapons: ["Knife", "Hammer", "Mace"]
+        },
         anomalyDescription: '* Ghosts in Twilight are attracted to the scene. They are usually malevolent or aroused to anger by the magic that draws them.' + '\n' +
-        '* Darkness becomes a palpable force, dimming the light even during day.' + '\n' +
-        '* Things become delicate and breakable (ignore one point of Durability and armor when striking objects).' + '\n' +
-        '* Spirit spells suffer a –2 penalty (Spirit is Stygia’s Inferior Arcanum)'
+            '* Darkness becomes a palpable force, dimming the light even during day.' + '\n' +
+            '* Things become delicate and breakable (ignore one point of Durability and armor when striking objects).' + '\n' +
+            '* Spirit spells suffer a –2 penalty (Spirit is Stygia’s Inferior Arcanum)'
     },
     {
         pathId: 'obrimos',
         fancyName: '⚡ Obrimos ✨',
-        realm: 'Aether',
+        name: "Obrimos",
+        aka: ["Theurgists"],
+        path: "the Mighty",
+        watchtower: "Golden Key",
+        realm: "the Aether",
+        kingdom: "Celestial Spheres",
+        abode: "Angels",
+        favouredResistance: "Resolve",
+        rulingArcana: ["Forces", "Prime"],
+        inferiorArcana: "Death",
+        concepts: [
+            "Soldier of God",
+            "Terrorist",
+            "Martial artist",
+            "Hyperdisciplined scholar of philology",
+            "Awakened cop",
+            "Defender of the weak",
+            "Contemporary inquisitor",
+            "Enforcer",
+            "Neighborhood hero",
+            "Uncompromising zealot",
+            "Monster hunter"
+        ],
+        summary: "Pursuing a divine mandate.",
+        magicalTools: {
+            wands: ["Steel", "Petrified wood", "Gold"],
+            weapons: ["Knife", "Spear", "Broadsword (or other doublebladed straight sword such as a Roman gladius)"]
+        },
         anomalyDescription: '* A storm brews or the weather acts crazy, raining hail in clear skies.' + '\n' +
-        '* The electrical system goes haywire, shorting out anything plugged into the local grid.' + '\n' +
-        '* Mana cannot be drawn from any Hallow in the area of the Anomaly for the duration of the Paradox.' + '\n' +
-        '* Tass cannot be converted to Mana in the area of the Anomaly for the duration of the Paradox.' + '\n' +
-        '* Mana cannot be solidified into tass in the area of the Anomaly for the duration of the Paradox.' + '\n' +
-        '* Resonance becomes negatively aspected. This is a lasting effect.' + '\n' +
-        '* Death spells suffer a –2 penalty (Death is the Aether’s Inferior Arcanum).'
+            '* The electrical system goes haywire, shorting out anything plugged into the local grid.' + '\n' +
+            '* Mana cannot be drawn from any Hallow in the area of the Anomaly for the duration of the Paradox.' + '\n' +
+            '* Tass cannot be converted to Mana in the area of the Anomaly for the duration of the Paradox.' + '\n' +
+            '* Mana cannot be solidified into tass in the area of the Anomaly for the duration of the Paradox.' + '\n' +
+            '* Resonance becomes negatively aspected. This is a lasting effect.' + '\n' +
+            '* Death spells suffer a –2 penalty (Death is the Aether’s Inferior Arcanum).'
     },
     {
         pathId: 'thyrsus',
         fancyName: '🌱 Thyrsus 🧞',
-        realm: 'Primal Wild',
+        name: "Thyrsus",
+        aka: ["Shamans"],
+        path: "Ecstasy",
+        watchtower: "Stone Book",
+        realm: "Primal Wild",
+        kingdom: "Totems",
+        abode: "Beasts",
+        favouredResistance: "Composure",
+        rulingArcana: ["Life", "Spirit"],
+        inferiorArcana: "Mind",
+        concepts: ["Native American healer", "visionary lunatic", "berserker", "enlightened veterinarian", "herbalist", "witch", "eco-saboteur"],
+        summary: "Passion and impulsive action.",
+        magicalTools: {
+            wands: ["Wood", "Copper", "Stone"],
+            weapons: ["Knife", "Spear", "Axe", "Sling"]
+        },
         anomalyDescription: '* Everyone on the scene becomes enervated, lacking energy. It is an effort to perform physical actions (–2 to all such rolls), even walking. Speed is halved.' + '\n' +
-        '* Flowers wither, milk curdles, animals are skittish and on edge.' + '\n' +
-        '* Some objects become ephemeral, existing only in Twilight. If 3+ dots were used in the spell that invoked the Paradox, objects might be transferred across the Gauntlet.' + '\n' +
-        '* Malevolent spirits in Twilight are attracted to the scene, or if 3+ dots were used in the spell that invoked the Paradox, they cross over from the Gauntlet.' + '\n' +
-        '* Slumbering spirits in objects awaken and turn against their wielders.' + '\n' +
-        '* Mind spells suffer a –2 penalty (Mind is the Primal Wild’s Inferior Arcanum).'
-    }
+            '* Flowers wither, milk curdles, animals are skittish and on edge.' + '\n' +
+            '* Some objects become ephemeral, existing only in Twilight. If 3+ dots were used in the spell that invoked the Paradox, objects might be transferred across the Gauntlet.' + '\n' +
+            '* Malevolent spirits in Twilight are attracted to the scene, or if 3+ dots were used in the spell that invoked the Paradox, they cross over from the Gauntlet.' + '\n' +
+            '* Slumbering spirits in objects awaken and turn against their wielders.' + '\n' +
+            '* Mind spells suffer a –2 penalty (Mind is the Primal Wild’s Inferior Arcanum).'
+    },
 ]
