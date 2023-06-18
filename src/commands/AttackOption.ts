119f54e1353414a67350070d7cfacbff5c1e2e33
James Hurburgh <JamesHurburgh@gmail.com>
2023-06-18 13:25:13 +1000

    refactor: Added filter and made constructor object based

--- src/commands/AttackOption.ts
+++ src/commands/AttackOption.ts
@@ -1,4 +1,10 @@
+import Attack from "./Attack";
 
+
+
+
+type ApplyFunction = (attack: Attack) => void
+type FilterFunction = (attack: Attack) => boolean
 
 export class AttackOption {
     id: string;
@@ -6,15 +12,37 @@ export class AttackOption {
     symbol: string;
     summary: string;
     description: string;
-    apply: Function;
+    apply: ApplyFunction;
+    _filter?: FilterFunction;
 
-    constructor(id: string, name: string, symbol: string, summary: string, description: string, apply: Function) {
-        this.id = id;
-        this.name = name;
-        this.symbol = symbol;
-        this.summary = summary;
-        this.description = description;
-        this.apply = apply;
+    constructor({
+        id,
+        name,
+        symbol,
+        summary,
+        description,
+        apply,
+        filter
+    }: { 
+        id: string,
+        name: string,
+        symbol: string,
+        summary: string,
+        description: string,
+        apply: ApplyFunction,
+        filter?: FilterFunction
+    }) {
+        this.id = id
+        this.name = name
+        this.symbol = symbol
+        this.summary = summary
+        this.description = description
+        this.apply = apply
+        this._filter = filter || undefined
     }
     fancyName() { return `${this.symbol} ${this.name}`; }
+    filter(attack: Attack) {
+        if(!this._filter) return false
+        return this._filter(attack)
+    }
 }
--- src/commands/AttackOptions.ts
+++ src/commands/AttackOptions.ts
@@ -2,36 +2,59 @@ import Attack from "./Attack";
 import { AttackOption } from "./AttackOption";
 
 export default [
-    new AttackOption(
-        'all-out-attack',
-        'All out Attack',
-        '💢',
-        '+2 to attack, lose defense',
-        'description',
-        (attack: Attack) => {
+    new AttackOption({
+        id: 'all-out-attack',
+        name: 'All out Attack',
+        symbol: '💢',
+        summary: '+2 to attack, lose defense',
+        description: 'description',
+        apply: (attack: Attack) => {
             attack.mods.push({ mod: 2, description: `💢 All out Attack` })
             attack.defenceLostTo = `💢 All out Attack`
-        }
-    ),
-    new AttackOption(
-        'willpower-attack',
-        'Attack with Willpower',
-        '😠',
-        '+3 to attack, -1 willpower',
-        'description',
-        (attack: Attack) => {
+        },
+        filter: (attack: Attack) => {
+            return (attack.attackType !== undefined) &&
+                (['weaponry', 'brawl'].indexOf(attack.attackType.skill.toLowerCase()) > -1) &&
+                (!attack.defenceLostTo)
+        },
+    }),
+    new AttackOption({
+        id: 'willpower-attack',
+        name: 'Attack with Willpower',
+        symbol: '😠',
+        summary: '+3 to attack, -1 willpower',
+        description: 'description',
+        apply: (attack: Attack) => {
             attack.mods.push({ mod: 3, description: `😠 Attack with Willpower` })
             attack.willpowerUsedOn = `😠 Attack with Willpower`
+        },
+        filter: (attack: Attack) => {
+            return !attack.willpowerUsedOn
         }
-    ),
-    new AttackOption(
-        'willpower-defense',
-        'Defend with Willpower',
-        '😣',
-        '-2 to attack',
-        'description',
-        (attack: Attack) => {
+    }),
+    new AttackOption({
+        id: 'willpower-defense',
+        name: 'Defend with Willpower',
+        symbol: '😣',
+        summary: '-2 to attack',
+        description: 'description',
+        apply: (attack: Attack) => {
             attack.mods.push({ mod: -2, description: `😣 Defend with Willpower` })
         }
-    )
+    }),
+    // new AttackOption({
+    //     id: 'aiming',
+    //     name: 'Aiming',
+    //     symbol: '🎯',
+    //     summary: '+1 per turn to a +3 maximum',
+    //     description: 'description',
+    //     apply: (attack: Attack) => {
+    //         attack.mods.push({ mod: -2, description: `🎯 Defend with Willpower` })
+    //     },
+    //     filter: (attack: Attack) => {
+    //         return (attack.attackType !== undefined) && (['weaponry', 'brawl'].indexOf(attack.attackType.skill.toLowerCase()) > -1)
+    //     },
+    //     minValue: 1,
+    //     maxValue: 3    
+    // }),
 ]
