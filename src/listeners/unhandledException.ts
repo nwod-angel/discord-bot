5a2f1fd969cf585be972559f406f9495b2846377
James Hurburgh <JamesHurburgh@gmail.com>
2024-08-04 20:52:35 +1000

    fix: Timestamp logs

--- src/listeners/unhandledException.ts
+++ src/listeners/unhandledException.ts
@@ -2,6 +2,7 @@ import { Client } from "discord.js";
 
 export default (client: Client): void => {
     client.on("unhandledException", error => {
+        console.log(Date.now())
         console.error('Unhandled exception:', error);
     });
 };
--- src/listeners/unhandledRejection.ts
+++ src/listeners/unhandledRejection.ts
@@ -2,6 +2,7 @@ import { Client } from "discord.js";
 
 export default (client: Client): void => {
     client.on("unhandledRejection", error => {
+        console.log(Date.now())
         console.error('Unhandled promise rejection:', error);
     });
 };
