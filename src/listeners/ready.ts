c55c168fd4788c2b41fe4f431f854aa32bb96d08
nwod angel <nwod.angel@gmail.com>
2026-06-20 23:15:51 +1000

    fix: load AutoCompleteCommands in ready handler
    The ready handler only loaded Commands, so getAutoCompleteCommands()
    threw when an autocomplete interaction arrived before loadAutoCompleteCommands()
    was called. Now both are loaded in parallel via Promise.all during the
    ready event.

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,5 +1,6 @@
 import { Client } from "discord.js";
 import { loadCommands } from "../Commands.js";
+import { loadAutoCompleteCommands } from "../AutoCompleteCommands.js";
 import { UpdateStatus } from "./UpdateStatus.js";
 import { logger } from "../logger.js";
 
@@ -9,7 +10,10 @@ export default (client: Client): void => {
             return;
         }
 
-        const commands = await loadCommands();
+        const [commands] = await Promise.all([
+            loadCommands(),
+            loadAutoCompleteCommands(),
+        ]);
         await client.application.commands.set(commands);
 
         logger.info({ username: client.user.username }, 'Bot is online');
